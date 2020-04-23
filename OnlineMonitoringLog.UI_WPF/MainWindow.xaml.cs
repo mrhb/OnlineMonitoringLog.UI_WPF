a888d3adc2b7826646140bbed654033ab216cbd9
mrhb <mohammad_hajjar@yahoo.com>
2020-04-23 12:17:01 +0430

    binding اولیه یک کلاس

--- OnlineMonitoringLog.UI_WPF/MainWindow.xaml.cs
+++ OnlineMonitoringLog.UI_WPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CollectionBinding;
 
 namespace OnlineMonitoringLog.UI_WPF
 {
@@ -20,9 +21,13 @@ namespace OnlineMonitoringLog.UI_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        Station MyFriends = new Station();
         public MainWindow()
         {
+            MyFriends.add();
             InitializeComponent();
+        
+
         }
     }
 }
